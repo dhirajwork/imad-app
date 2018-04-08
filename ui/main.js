7b4e2d91364b66e9cfcfa779302eac557c2f827e
Dhiraj Jaiswal <dhiraj.jaiswal96@gmail.com>
2018-04-08 12:52:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,46 +1,42 @@
-console.log('Loaded!');
-//submit username/password to login
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-
-//make a request to the server and send the names
-//create a request object
-
-var request = new XMLHttpRequest();
-
-//capture the response and store it in a variable
-
-request.onreadystatechange = function(){
-if(request.readyState === XMLHttpRequest.DONE){
-//Take action
-
-if(request.status === 200){
-//capture a list of names and render it as list
-
-console.log('user logged in');
-alert('logged in successfully!!');
-}
-else if(request.status === 403){
-alert('username/password incorrect!!');
-}
-else if(request.status === 500){
-alert('something went wrong');
-}
-//Not done yet
-}
-};
-
-
-//Make the request
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
-console.log(username);
-console.log(password);
-request.open('POST', 'http://dhirajjaiswal96.imad.hasura-app.io/login', true);
-request.setRequestHeader('Content-Type', 'application/json');
-request.send(JSON.stringify({username: username, password: password}));
-
-
-
-
+    //make a request to the server and send the names
+    //create a request object
+    
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take action
+            
+            if(request.status === 200){
+                //capture a list of names and render it as list
+    
+              console.log('user logged in');
+              alert('logged in successfully!!');
+                }
+                else if(request.status === 403){
+                    alert('username/password incorrect!!');
+                }
+                else if(request.status === 500){
+                    alert('something went wrong');
+                }
+        //Not done yet
+    }
+    };
+    
+    //Make the request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://dhirajjaiswal96.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+    
+        
+    
+    
 };
