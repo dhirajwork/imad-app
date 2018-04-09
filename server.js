f55160c1117c6c21193bf55f7675b1c7da7e1d2c
Dhiraj Jaiswal <dhiraj.jaiswal96@gmail.com>
2018-04-09 14:08:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.post('/login', function (req, res) {
 
 app.get('/article/:articleName' ,function(req,res)
 {
-    pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName"'",function(err,result)
+    pool.query("SELECT * FROM article WHERE title = '" +req.params.articleName +"'",function(err,result)
     {
         if(err){
             res.status(500).send(err.toString());
