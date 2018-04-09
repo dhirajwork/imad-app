685528748e9a09bde0f74acd99dfe671a479f23f
Dhiraj Jaiswal <dhiraj.jaiswal96@gmail.com>
2018-04-09 09:49:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,10 @@ app.post('/create-user', function (req, res) {
             res.status(500).send(err.toString());
         }
         else{
-            res.send('User successfully created: ' + username);
+            var result2 = {"result" :"User successfully created",
+                "name":"'+username+'"
+            };
+            res.send(result);
         }
   });
   
