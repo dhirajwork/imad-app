c2abae5254242ef286896ab2f00368dba843f670
Dhiraj Jaiswal <dhiraj.jaiswal96@gmail.com>
2018-04-09 10:46:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,7 +93,8 @@ app.post('/create-user', function (req, res) {
   var dbString = hash(password, salt);
   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result){
        if(err){
-            res.status(500).send(err.toString());
+           var error3 = {"error" : err.toString};
+            res.status(500).send(error3);
         }
         else{
             var result2 = {"result" :"User successfully created",
