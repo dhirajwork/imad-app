ddef621fa0bc66433ff6a75d1872922368c76f1a
Dhiraj Jaiswal <dhiraj.jaiswal96@gmail.com>
2018-04-09 10:49:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,7 +93,10 @@ app.post('/create-user', function (req, res) {
   var dbString = hash(password, salt);
   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result){
        if(err){
-           var error3 = {"error":err.toString};
+           var error3 = {"hi": "try with diff name",
+               "error":err.toString()
+               
+           };
             res.status(500).send(error3);
         }
         else{
